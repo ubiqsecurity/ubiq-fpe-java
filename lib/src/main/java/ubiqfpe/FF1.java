be0f94478460aa1ee9a9bd785e9e9b6e755b1620
John Tyner <jtyner@ubiqsecurity.com>
2021-04-21 11:01:19 -0700

    initial (working) implementation of ff1

---
+++ lib/src/main/java/ubiqfpe/FF1.java
@@ -0,0 +1,155 @@
+package ubiqfpe;
+
+import java.lang.Math;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class FF1
+{
+    private FFX ctx;
+
+    public FF1(final byte[] key, final byte[] twk,
+               final long twkmin, final long twkmax,
+               final int radix) {
+        ctx = new FFX(key, twk, (long)1 << 32, twkmin, twkmax, radix);
+    }
+
+    private String cipher(String X, byte[] twk, final boolean encrypt) {
+        final int n = X.length();
+        final int u = n / 2, v = n - u;
+
+        final int b = ((int)Math.ceil(
+                           (Math.log(ctx.radix) / Math.log(2)) * v) + 7) / 8;
+        final int d = 4 * ((b + 3) / 4) + 4;
+
+        final int p = 16;
+        final int r = ((d + 15) / 16) * 16;
+
+        BigInteger c, y;
+        String A, B, C, Y;
+        byte[] PQ, R;
+        int q;
+
+        if (twk == null) {
+            twk = ctx.twk;
+        }
+
+        if (n < ctx.txtmin || n > ctx.txtmax) {
+            throw new IllegalArgumentException("invalid input length");
+        } else if (twk.length < ctx.twkmin ||
+                   (ctx.twkmax > 0 && twk.length > ctx.twkmax)) {
+            throw new IllegalArgumentException("invalid tweak length");
+        }
+
+        q = ((twk.length + b + 1 + 15) / 16) * 16;
+
+        PQ = new byte[p + q];
+        R  = new byte[r];
+
+        if (encrypt) {
+            A = X.substring(0, u);
+            B = X.substring(u);
+        } else {
+            B = X.substring(0, u);
+            A = X.substring(u);
+        }
+
+        PQ[0]  = 1;
+        PQ[1]  = 2;
+        PQ[2]  = 1;
+        PQ[3]  = (byte)(ctx.radix >> 16);
+        PQ[4]  = (byte)(ctx.radix >>  8);
+        PQ[5]  = (byte)(ctx.radix >>  0);
+        PQ[6]  = 10;
+        PQ[7]  = (byte)u;
+        PQ[8]  = (byte)(n >> 24);
+        PQ[9]  = (byte)(n >> 16);
+        PQ[10] = (byte)(n >>  8);
+        PQ[11] = (byte)(n >>  0);
+        PQ[12] = (byte)(twk.length >> 24);
+        PQ[13] = (byte)(twk.length >> 16);
+        PQ[14] = (byte)(twk.length >>  8);
+        PQ[15] = (byte)(twk.length >>  0);
+
+        System.arraycopy(twk, 0, PQ, p, twk.length);
+        /* remainder of Q already initialized to 0 */
+
+        for (int i = 0; i < 10; i++) {
+            final int m = (((i + (encrypt ? 1 : 0)) % 2) == 1) ? u : v;
+
+            byte[] numb;
+            String tmp;
+
+            PQ[p + q - b - 1] = (byte)(encrypt ? i : (9 - i));
+
+            c = new BigInteger(B, ctx.radix);
+            numb = c.toByteArray();
+            if (b <= numb.length) {
+                System.arraycopy(numb, 0, PQ, p + q - b, b);
+            } else {
+                for (int j = 0; j < b - numb.length; j++) {
+                    PQ[p + q - b + j] = 0;
+                }
+                System.arraycopy(numb, 0, PQ, p + q - numb.length, numb.length);
+            }
+
+            ctx.prf(R, 0, PQ, 0);
+
+            for (int j = 1; j < r / 16; j++) {
+                final int l = j * 16;
+
+                Arrays.fill(R, l, l + 12, (byte)0);
+                R[l + 12] = (byte)(j >> 24);
+                R[l + 13] = (byte)(j >> 16);
+                R[l + 14] = (byte)(j >>  8);
+                R[l + 15] = (byte)(j >>  0);
+
+                FFX.xor(R, l, R, 0, R, l, 16);
+
+                ctx.ciph(R, l, R, l);
+            }
+
+            y = new BigInteger(Arrays.copyOf(R, d));
+            y = y.mod(BigInteger.ONE.shiftLeft(8 * d));
+
+            c = new BigInteger(A, ctx.radix);
+            if (encrypt) {
+                c = c.add(y);
+            } else {
+                c = c.subtract(y);
+            }
+
+            c = c.mod(BigInteger.valueOf(ctx.radix).pow(m));
+            C = FFX.str(m, ctx.radix, c);
+
+            tmp = A;
+            A = B;
+            B = C;
+            C = tmp;
+        }
+
+        if (encrypt) {
+            Y = A + B;
+        } else {
+            Y = B + A;
+        }
+
+        return Y;
+    }
+
+    public String encrypt(String X, byte[] twk) {
+        return this.cipher(X, twk, true);
+    }
+
+    public String encrypt(String X) {
+        return this.encrypt(X, null);
+    }
+
+    public String decrypt(String X, byte[] twk) {
+        return this.cipher(X, twk, false);
+    }
+
+    public String decrypt(String X) {
+        return this.decrypt(X, null);
+    }
+}
---
+++ lib/src/test/java/ubiqfpe/FF1Test.java
@@ -0,0 +1,116 @@
+package ubiqfpe;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FF1Test
+{
+    private void test(final byte[] key, final byte[] twk,
+                      final String PT, final String CT,
+                      final int radix) {
+        String out;
+        FF1 ctx;
+
+        assertEquals(PT.length(), CT.length());
+
+        ctx = new FF1(key, twk, 0, 0, radix);
+
+        out = ctx.encrypt(PT, twk);
+        assertEquals(CT, out);
+
+        out = ctx.decrypt(CT, twk);
+        assertEquals(PT, out);
+    }
+
+    private final byte[] key = {
+        (byte)0x2b, (byte)0x7e, (byte)0x15, (byte)0x16,
+        (byte)0x28, (byte)0xae, (byte)0xd2, (byte)0xa6,
+        (byte)0xab, (byte)0xf7, (byte)0x15, (byte)0x88,
+        (byte)0x09, (byte)0xcf, (byte)0x4f, (byte)0x3c,
+        (byte)0xef, (byte)0x43, (byte)0x59, (byte)0xd8,
+        (byte)0xd5, (byte)0x80, (byte)0xaa, (byte)0x4f,
+        (byte)0x7f, (byte)0x03, (byte)0x6d, (byte)0x6f,
+        (byte)0x04, (byte)0xfc, (byte)0x6a, (byte)0x94,
+    };
+
+    private final byte[] twk1 = {};
+    private final byte[] twk2 = {
+        (byte)0x39, (byte)0x38, (byte)0x37, (byte)0x36,
+        (byte)0x35, (byte)0x34, (byte)0x33, (byte)0x32,
+        (byte)0x31, (byte)0x30,
+    };
+    private final byte[] twk3 = {
+        (byte)0x37, (byte)0x37, (byte)0x37, (byte)0x37,
+        (byte)0x70, (byte)0x71, (byte)0x72, (byte)0x73,
+        (byte)0x37, (byte)0x37, (byte)0x37,
+    };
+
+    private final String[] PT = {
+        "0123456789", "0123456789abcdefghi"
+    };
+
+    @Test
+    public void nist1() {
+        this.test(Arrays.copyOf(this.key, 16),
+                  this.twk1,
+                  PT[0], "2433477484", 10);
+    }
+
+    @Test
+    public void nist2() {
+        this.test(Arrays.copyOf(this.key, 16),
+                  this.twk2,
+                  PT[0], "6124200773", 10);
+    }
+
+    @Test
+    public void nist3() {
+        test(Arrays.copyOf(this.key, 16),
+             this.twk3,
+             PT[1], "a9tv40mll9kdu509eum", 36);
+    }
+
+    @Test
+    public void nist4() {
+        test(Arrays.copyOf(this.key, 24),
+             this.twk1,
+             PT[0], "2830668132", 10);
+    }
+
+    @Test
+    public void nist5() {
+        test(Arrays.copyOf(this.key, 24),
+             this.twk2,
+             PT[0], "2496655549", 10);
+    }
+
+    @Test
+    public void nist6() {
+        test(Arrays.copyOf(this.key, 24),
+             this.twk3,
+             PT[1], "xbj3kv35jrawxv32ysr", 36);
+    }
+
+    @Test
+    public void nist7() {
+        test(Arrays.copyOf(this.key, 32),
+             this.twk1,
+             PT[0], "6657667009", 10);
+    }
+
+    @Test
+    public void nist8() {
+        test(Arrays.copyOf(this.key, 32),
+             this.twk2,
+             PT[0], "1001623463", 10);
+    }
+
+    @Test
+    public void nist9() {
+        test(Arrays.copyOf(this.key, 32),
+             this.twk3,
+             PT[1], "xs8a0azh2avyalyzuwd", 36);
+    }
+}
