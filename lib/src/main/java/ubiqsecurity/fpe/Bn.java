be62a9379faa93975f66d7de8b7e700fdb7d54e9
Anthony Iasi <anthony@fhoosh.com>
2021-07-19 07:09:42 -0700

    Initial checkin

---
+++ lib/src/main/java/ubiqsecurity/fpe/Bn.java
@@ -0,0 +1,168 @@
+package ubiqsecurity.fpe;
+
+import java.io.IOException ;
+import java.math.BigInteger;
+ 
+
+public class Bn {
+
+    /*
+    * Convert a numerical value in a given alphabet to a number
+    *
+    * An alphabet consists of single-byte symbols in which each
+    * symbol represents the numerical value associated with its
+    * index/position in the alphabet. for example, consider the
+    * alphabet:
+    *   !@#$%^&*()
+    * In this alphabet ! occupies index 0 and is therefore
+    * assigned that value. @ = 1, # = 2, etc. Furthermore, the
+    * alphabet contains 10 characters, so that becomes the radix
+    * of the input. Using the alphabet above, an input of @$#
+    * translates to a value of 132 (one hundred thirty-two,
+    * decimal).
+    *
+    * If the alphabet above were instead:
+    *   !@#$%^&*
+    * The radix would be 8 and an input of @$# translates to a
+    * value of 90 (ninety, decimal).
+    *
+    * The function returns the numerical value of the str pattern 
+    * position found in the alphabet
+    */    
+    public static BigInteger __bigint_set_str(final String str, final String alpha)
+        {
+            final int len = str.length();
+            /*
+             * the alphabet can be anything and doesn't have
+             * to be in a recognized canonical order. the only
+             * requirement is that every value in the list be
+             * unique. checking that constraint is an expensive
+             * undertaking, so it is assumed. as such, the radix
+             * is simply the number of characters in the alphabet.
+             */
+            final int rad = alpha.length();
+
+            BigInteger m, a;
+            int i;
+            BigInteger x;
+            
+            /* represents the numerical value of str */
+            x = BigInteger.valueOf(0);
+            /*
+             * multiplier used to multiply each digit
+             * of the input into its correct position
+             */
+            m = BigInteger.valueOf(1);
+            
+            for (i = 0; i < len; i++) {
+                final int pos;
+                /*
+                 * determine index/position in the alphabet.
+                 * if the character is not present the input
+                 * is not valid.
+                 */
+                pos = alpha.indexOf(str.charAt(len - 1 - i));
+                if (pos < 0) {
+                    throw new IllegalArgumentException("invalid argument, position in alphabet not found");
+                }
+                /*
+                 * multiply the digit into the correct position
+                 * and add it to the result
+                 */
+                a = m.multiply(BigInteger.valueOf(pos));
+                x = x.add(a);
+                m = m.multiply(BigInteger.valueOf(rad));
+            }
+            return x;
+        }
+    
+    
+    /*
+     * Convenience function returns String with inserted char at position
+     */     
+    public static String insertChar(String str, char ch, int position) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.insert(position, ch);
+        return sb.toString();
+    }
+    
+    
+    
+    /*
+     * This function returns the str pattern of the alphabet given the numeric
+     * positional value
+     */    
+    public static String __bigint_get_str(final String alpha, final BigInteger x) {
+        final int rad = alpha.length();
+        BigInteger quotient;
+        int i = 0;
+        String str = "";
+        
+        /*
+         * to convert the numerical value, repeatedly
+         * divide (storing the resulted quotient and the remainder)
+         * by the desired radix of the output.
+         *
+         * the remainder is the current digit; the result
+         * of the division becomes the input to the next
+         * iteration
+         */
+        final BigInteger len = BigInteger.valueOf(alpha.length());
+        BigInteger positional = BigInteger.valueOf(1);
+        quotient = x.multiply(positional);
+        
+        while (quotient.compareTo(BigInteger.valueOf(0)) != 0) {
+            int remainder;
+            
+            BigInteger result[] = quotient.divideAndRemainder(BigInteger.valueOf(rad));
+            remainder = result[1].intValue();
+            quotient=  BigInteger.valueOf(result[0].intValue());
+            if (i < len.intValue()) {
+                str = insertChar(str, alpha.charAt(remainder), i);
+            }
+            i++;
+        }
+        
+        /* handle the case where the initial value was 0 */
+        if (i <= 0) {
+            if (i < len.intValue()) {
+                str = insertChar(str, alpha.charAt(0), i);
+            }
+            i++;
+        }
+        
+        if (i <= len.intValue()) {
+            /*
+             * to simplify conversion from a number to a string,
+             * the output digits are stored in reverse order.
+             * reverse the final value so that the output is correct
+             */
+            StringBuilder revStr = new StringBuilder();
+            revStr.append(str);
+            revStr.reverse();
+            str = revStr.toString();
+        }
+          
+        return str;
+    }
+
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
