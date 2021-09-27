3e61e931726f9bd4b68f255f6e401935a875d9c9
John Tyner <jtyner@ubiqsecurity.com>
2021-09-27 10:29:16 -0400

    add fix for big integer export and test that triggers the bug

--- lib/src/main/java/ubiqsecurity/fpe/FF1.java
+++ lib/src/main/java/ubiqsecurity/fpe/FF1.java
@@ -119,6 +119,17 @@ public class FF1 extends FFX
              */
             c = new BigInteger(B, this.radix);
             numb = c.toByteArray();
+            if (numb[0] == 0 && numb.length > 1) {
+                /*
+                 * Per the Java documentation, BigInteger.toByteArray always
+                 * returns enough bytes to contain a sign bit. For the purposes
+                 * of this function all numbers are unsigned; however, when the
+                 * most-significant bit is set in a number, the Java library
+                 * returns an extra most-significant byte that is set to 0.
+                 * That byte must be removed for the cipher to work correctly.
+                 */
+                numb = Arrays.copyOfRange(numb, 1, numb.length);
+            }
             if (b <= numb.length) {
                 System.arraycopy(numb, 0, PQ, PQ.length - b, b);
             } else {
